af2bb93c680c78db45a002932edeb5a815ab68e3
particalmist <jasesans@outlook.com>
2024-03-08 23:55:05 -0800

    secondpush

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/navbar/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <NavBar />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + REACT</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,32 @@
+import { SouthRounded } from '@mui/icons-material';
+import logo_alienblaster from '../../assets/logo_alienblaster.png'
+import './NavBar.css'
+import SoundSlider001 from '../sliders/SoundSlider001';
+
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+        <div>
+            <SoundSlider001 />      
+        </div>
+      <div className="navbar__logo">
+        <img src={logo_alienblaster} alt="logo" />
+      </div>
+      <div className="navbar__menu">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+export default NavBar;
