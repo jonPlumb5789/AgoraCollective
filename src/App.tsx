bb6cea74046a723e76be7e1bd4d6bf292e9e8da4
jonPlumb5789 <jonplumb89@outlook.com>
2024-03-18 13:20:13 -0400

    brainspaceAdd

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import Home from './components/Home/Home'
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
 import Gallery from './components/Gallery/Gallery'
+import BrainSpace from './components/BrainSpace/BrainSpace'
+import Butler from './components/AiAssisstant/Butler'
 
 function App() {
 
@@ -17,11 +19,14 @@ function App() {
       <NavBar />
       <div>
         <Routes>
-          <Route path="/" element={<Home />} /> // Add the 'Home' component
-          <Route path="/signin" element={<SignIn />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/butler" element={<Butler />} />
           <Route path="/gallery" element={<Gallery />} />
+          <Route path="/brainspace" element={<BrainSpace />} />
+
         </Routes>
       </div>
     </>
